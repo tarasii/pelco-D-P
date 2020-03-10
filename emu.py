a51bf677613c5414f8cf0b4b5b4e7ffc1265897e
tarasii <tarasii@gmail.com>
2020-03-10 10:15:41 +0200

    tk emulator

---
+++ emu.py
@@ -0,0 +1,186 @@
+import Tkinter as tk
+import ptz.pelco
+
+addr = 0
+buf = 0
+
+pos = 0
+
+root = tk.Tk()
+frame = tk.Frame(root)
+frame.pack()
+
+mouse = False  
+x = 0
+y = 0
+
+v_addr = tk.StringVar()
+e_addr = tk.Entry(frame, textvariable=v_addr)
+e_addr.grid(column=0, row=0, columnspan=4)
+v_addr.set("addr:{:03d} buf:{:03d}".format(addr, buf))
+
+
+def motion(event):
+  global mouse
+  global x
+  global y
+
+  x_max = 130 #c.winfo_width()
+  y_max = 130 #c.winfo_height()
+
+  x_cur = event.x
+  y_cur = event.y
+
+  if mouse and x_cur < x_max and y_cur < y_max: 
+    #print("Mouse position: (%s %s)" % (event.x, event.y))
+    x = (x_cur - x_max / 2) * 256 / x_max 
+    y = (y_cur - y_max / 2) * 256 / y_max 
+    #x = x_cur
+    #y = y_cur
+    side = 'left'
+    if x > 0: 
+      side = 'right'
+    cmd = ptz.pelco.d.cmd_encode(addr,side,[abs(x),abs(y)])
+    v_cmd.set(cmd.encode('hex'))
+    #v_cmd.set("x:{} y:{}".format(x,y))
+    v_lbl.set("x:{} y:{}".format(x,y))
+
+    tmp = float(y_cur)/(1.2*float(y_max))
+    sby.set(tmp, 0.2+tmp)
+    tmp = float(x_cur)/(1.2*float(x_max))
+    sbx.set(tmp, 0.2+tmp)
+  return
+
+def mouse_down(event):
+  global mouse
+  mouse = True
+
+def mouse_up(event):
+  global mouse
+  global x
+  global y
+  global addr
+  mouse = False
+  x = 0
+  y = 0
+  cmd = ptz.pelco.d.cmd_encode(addr,'stop',[0,0])
+  v_cmd.set(cmd.encode('hex'))
+  v_lbl.set("x:{} y:{}".format(x,y))
+
+frc = tk.Frame(frame, width=130, height=130)
+frc.grid(column=5, row=0, rowspan=5)
+
+sby = tk.Scrollbar(frc)  
+sby.pack( side = tk.RIGHT, fill = tk.Y )
+sby.set(0.4,0.6)
+
+sbx = tk.Scrollbar(frc, orient = tk.HORIZONTAL)  
+sbx.pack( side = tk.BOTTOM, fill = tk.X )
+sbx.set(0.4,0.6)
+#sbx.set(0.0,0.2)
+
+#print dir(sbx)
+
+c = tk.Canvas(frc, width=130, height=130, bg='black')
+#c.grid(column=5, row=0, rowspan=5)
+c.bind('<Motion>',motion)
+c.bind('<ButtonPress-1>', mouse_down)
+c.bind('<ButtonRelease-1>', mouse_up)
+c.pack(side=tk.LEFT,expand=True,fill=tk.BOTH)
+#print dir(c)
+#print   c.winfo_width(),c.winfo_height()
+
+v_lbl = tk.StringVar()
+lbl = tk.Label(frame, textvariable=v_lbl, bg='black', fg='white')
+v_lbl.set("x:{} y:{}".format(x,y))
+lbl.grid(column=5, row=0)
+
+
+
+
+def onNumbersButton(num):
+  global pos
+  global buf
+
+  if pos < 3:
+  	pos = pos + 1  
+  else:
+    pos = 0
+    buf = 0
+
+  buf = buf*10 + num
+  v_addr.set("addr:{:03d} buf:{:03d}".format(addr, buf))
+
+def onCamButton():
+  global addr
+  global buf
+
+  addr = buf
+  v_addr.set("addr:{:03d} buf:{:03d}".format(addr, buf))
+
+b_cam = tk.Button(frame, text="OK", width=5, command=onCamButton)
+b_cam.grid(column=1, row=4, columnspan=2)
+
+b_0 = tk.Button(frame, text="0", command=lambda:onNumbersButton(0))
+b_0.grid(column=0, row=4)
+
+
+b_1 = tk.Button(frame, text="1", command=lambda:onNumbersButton(1))
+b_1.grid(column=0, row=3)
+
+b_2 = tk.Button(frame, text="2", command=lambda:onNumbersButton(2))
+b_2.grid(column=1, row=3)
+
+b_3 = tk.Button(frame, text="3", command=lambda:onNumbersButton(3))
+b_3.grid(column=2, row=3)
+
+
+b_4 = tk.Button(frame, text="4", command=lambda:onNumbersButton(4))
+b_4.grid(column=0, row=2)
+
+b_5 = tk.Button(frame, text="5", command=lambda:onNumbersButton(5))
+b_5.grid(column=1, row=2)
+
+b_6 = tk.Button(frame, text="6", command=lambda:onNumbersButton(6))
+b_6.grid(column=2, row=2)
+
+b_7 = tk.Button(frame, text="7", command=lambda:onNumbersButton(7))
+b_7.grid(column=0, row=1)
+
+b_8 = tk.Button(frame, text="8", command=lambda:onNumbersButton(8))
+b_8.grid(column=1, row=1)
+
+b_9 = tk.Button(frame, text="9", command=lambda:onNumbersButton(9))
+b_9.grid(column=2, row=1)
+
+b_close = tk.Button(frame, text="CLOSE", width=5, command=lambda:onCmdButton("close"))
+b_close.grid(column=3, row=2)
+
+b_open = tk.Button(frame, text="OPEN", width=5, command=lambda:onCmdButton("open"))
+b_open.grid(column=4, row=2)
+
+b_near = tk.Button(frame, text="NEAR", width=5, command=lambda:onCmdButton("near"))
+b_near.grid(column=3, row=3)
+
+b_far = tk.Button(frame, text="FAR", width=5, command=lambda:onCmdButton("far"))
+b_far.grid(column=4, row=3)
+
+def onCmdButton(txt):
+	global addr
+	cmd = ptz.pelco.d.cmd_encode(addr,txt,[0,0])
+  #print ("fuck")
+	v_cmd.set(cmd.encode('hex'))
+
+b_wide = tk.Button(frame, text="WIDE", width=5, command=lambda:onCmdButton("wide"))
+b_wide.grid(column=3, row=4)
+
+b_tele = tk.Button(frame, text="TELE", width=5, command=lambda:onCmdButton("tele"))
+b_tele.grid(column=4, row=4)
+
+v_cmd = tk.StringVar()
+e_cmd = tk.Entry(frame, textvariable=v_cmd)
+e_cmd.grid(column=0, row=5, columnspan=5)
+v_cmd.set("Hello!")
+
+
+root.mainloop()
